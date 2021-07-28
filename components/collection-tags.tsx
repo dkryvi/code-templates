b2d762272cce7f32b9adc6379189d47d3b1fa0e1
dkryvi <krivichaninds@gmail.com>
2021-07-28 12:34:36 +0300

    Update collection tags color scheme

--- components/collection-tags.tsx
+++ components/collection-tags.tsx
@@ -17,8 +17,10 @@ const CollectionTags: React.FC<Props> = ({tags, activeTag, onTagClick}) => {
         <li
           key={tag}
           className={clsx([
-            'mx-1 my-1 px-4 py-2 transition bg-black text-white rounded-full cursor-pointer hover:opacity-80',
-            activeTag === tag && 'bg-green-500'
+            'mx-1 my-1 px-4 py-2 transition rounded-full border-2 cursor-pointer',
+            activeTag === tag
+              ? 'bg-black border-black text-white hover:opacity-70'
+              : 'bg-gray-100 border-gray-200 text-black hover:bg-gray-200 hover:border-gray-300'
           ])}
           onClick={handleClick(tag)}
         >
