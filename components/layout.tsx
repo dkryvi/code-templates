d4358f7ee6256eab42a4ad133ad38d3f9fbc5d0b
dkryvi <krivichaninds@gmail.com>
2021-08-03 17:20:37 +0300

    Show FAB buttons on the screen (#14)
    * Show FAB buttons on the screen
    
    * Use icons from package

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,19 +1,44 @@
-import Alert from './alert'
-import Footer from './footer'
-import Meta from './meta'
+import {ArrowUpIcon, ClipboardCopyIcon} from '@heroicons/react/outline'
+
+import Alert from 'components/alert'
+import Footer from 'components/footer'
+import Meta from 'components/meta'
+import copyToClipboard from 'lib/utils/copy-to-clipboard'
 
 type Props = {
   children: React.ReactNode
 }
 
 const Layout: React.FC<Props> = ({children}) => {
+  const scrollToTop = () =>
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'})
+
+  const copyLink = () => {
+    copyToClipboard(window.location.href)
+    alert('Link copied to clipboard')
+  }
+
   return (
     <>
       <Meta />
-      <div className="min-h-screen">
+      <main className="min-h-screen">
         <Alert />
-        <main>{children}</main>
-      </div>
+        {children}
+        <div className="fixed right-8 bottom-8 flex flex-col space-y-4">
+          <button
+            className="p-4 rounded-full bg-white shadow-md hover:shadow-xl"
+            onClick={scrollToTop}
+          >
+            <ArrowUpIcon className="w-6 h-6" />
+          </button>
+          <button
+            className="p-4 rounded-full bg-white shadow-md hover:shadow-xl"
+            onClick={copyLink}
+          >
+            <ClipboardCopyIcon className="w-6 h-6" />
+          </button>
+        </div>
+      </main>
       <Footer />
     </>
   )
