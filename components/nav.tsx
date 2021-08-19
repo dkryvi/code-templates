e410d82f8c066833865a7af48825579b7cdbdbb7
dkryvi <krivichaninds@gmail.com>
2021-08-19 17:14:15 +0300

    Update autocomplete place in the nav

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,9 +1,11 @@
-import {Fragment} from 'react'
+import React, {Fragment} from 'react'
 import Link from 'next/link'
 import {useRouter} from 'next/router'
-import {Disclosure} from '@headlessui/react'
+import {Disclosure, Transition} from '@headlessui/react'
 import {MenuIcon, XIcon} from '@heroicons/react/outline'
 import clsx from 'clsx'
+
+import Autocomplete from 'components/autocomplete'
 
 const navigation = [
   {name: 'Home', href: '/'},
@@ -15,12 +17,12 @@ const Nav: React.FC = () => {
   const router = useRouter()
 
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-gray-800 sticky top-0 z-50">
       {({open}) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
@@ -54,29 +56,49 @@ const Nav: React.FC = () => {
                   </div>
                 </div>
               </div>
+              <div className="hidden sm:block max-w-xs lg:max-w-lg w-full">
+                <Autocomplete containerId="screen-autocomplete" />
+              </div>
             </div>
           </div>
-          <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
+          <Transition
+            show={open}
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+          >
+            <Disclosure.Panel
+              as="ul"
+              className="sm:hidden z-50 absolute w-full bg-gray-800 px-2 pt-2 pb-3 space-y-1 "
+              unmount
+            >
+              <div className="mb-4 w-full">
+                <Autocomplete containerId="mobile-autocomplete" />
+              </div>
               {navigation.map((item) => (
-                <Link key={item.name} href={item.href}>
-                  <a
-                    className={clsx([
-                      router.pathname === item.href
-                        ? 'bg-gray-900 text-white'
-                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'block px-3 py-2 rounded-md text-base font-medium'
-                    ])}
-                    aria-current={
-                      router.pathname === item.href ? 'page' : undefined
-                    }
-                  >
-                    {item.name}
-                  </a>
-                </Link>
+                <li key={item.name}>
+                  <Link href={item.href}>
+                    <a
+                      className={clsx([
+                        router.pathname === item.href
+                          ? 'bg-gray-900 text-white'
+                          : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'block px-3 py-2 rounded-md text-base font-medium'
+                      ])}
+                      aria-current={
+                        router.pathname === item.href ? 'page' : undefined
+                      }
+                    >
+                      {item.name}
+                    </a>
+                  </Link>
+                </li>
               ))}
-            </div>
-          </Disclosure.Panel>
+            </Disclosure.Panel>
+          </Transition>
         </>
       )}
     </Disclosure>
