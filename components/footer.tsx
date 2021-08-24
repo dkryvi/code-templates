361b3973a26eb76b85a785bbd3460d2ca1888e25
dkryvi <krivichaninds@gmail.com>
2021-08-24 17:31:50 +0300

    Open footer links in the new tab

--- components/footer.tsx
+++ components/footer.tsx
@@ -13,11 +13,18 @@ const Footer: React.FC = () => {
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="https://nextjs.org/docs/basic-features/pages"
+              target="_blank"
+              rel="noreferrer"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Read Documentation
             </a>
-            <a href={REPOSITORY_URL} className="mx-3 font-bold hover:underline">
+            <a
+              href={REPOSITORY_URL}
+              target="_blank"
+              rel="noreferrer"
+              className="mx-3 font-bold hover:underline"
+            >
               View on GitHub
             </a>
           </div>
