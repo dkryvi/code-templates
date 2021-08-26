fbc67f931e9145a1668aa5b8931b1ef777c37847
dkryvi <krivichaninds@gmail.com>
2021-08-26 10:57:05 +0300

    Update parsing flow

--- lib/utils/markdown-to-html.ts
+++ lib/utils/markdown-to-html.ts
@@ -1,10 +1,20 @@
-import remark from 'remark'
-import html from 'remark-html'
-import prism from 'remark-prism'
+import rehypePrism from '@mapbox/rehype-prism'
+import html from 'rehype-stringify'
+import gfm from 'remark-gfm'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import {unified} from 'unified'
 
 export default async function markdownToHtml(
   markdown: string
 ): Promise<string> {
-  const result = await remark().use(html).use(prism).process(markdown)
+  const result = await unified()
+    .use(remarkParse)
+    .use(gfm)
+    .use(remarkRehype)
+    .use(rehypePrism)
+    .use(html)
+    .process(markdown)
+
   return result.toString()
 }
