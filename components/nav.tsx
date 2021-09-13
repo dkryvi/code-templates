3864f826c9eeda73e03cc2c968723e99e77d580a
dkryvi <krivichaninds@gmail.com>
2021-09-13 21:31:52 +0300

    Update menu icon hover style

--- components/nav.tsx
+++ components/nav.tsx
@@ -22,25 +22,20 @@ const Nav: React.FC = () => {
       as="nav"
       className="bg-white sticky top-0 z-50 border-b border-accent-2"
     >
-      {({open}) => (
-        <>
+      {({open}) => {
+        const Icon = open ? XIcon : MenuIcon;
+
+        return   <>
           <Container>
             <div className="relative flex items-center justify-between h-16">
               <div className="flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md text-gray-900 hover:text-white hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XIcon
-                      className="block h-6 w-6 text-gray-900"
+                    <Icon
+                      className="block h-6 w-6 text-current"
                       aria-hidden="true"
                     />
-                  ) : (
-                    <MenuIcon
-                      className="block h-6 w-6 text-gray-900"
-                      aria-hidden="true"
-                    />
-                  )}
                 </Disclosure.Button>
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
@@ -107,7 +102,7 @@ const Nav: React.FC = () => {
             </Disclosure.Panel>
           </Transition>
         </>
-      )}
+      }}
     </Disclosure>
   )
 }
