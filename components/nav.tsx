f0c1809487d458bdc73f167b677ef05fbf2fd950
dkryvi <krivichaninds@gmail.com>
2021-09-14 07:49:08 +0300

    Update menu icon hover style (#39)
    * Update menu icon hover style
    
    * Fix nav formatting

--- components/nav.tsx
+++ components/nav.tsx
@@ -22,92 +22,89 @@ const Nav: React.FC = () => {
       as="nav"
       className="bg-white sticky top-0 z-50 border-b border-accent-2"
     >
-      {({open}) => (
-        <>
-          <Container>
-            <div className="relative flex items-center justify-between h-16">
-              <div className="flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XIcon
-                      className="block h-6 w-6 text-gray-900"
+      {({open}) => {
+        const Icon = open ? XIcon : MenuIcon
+
+        return (
+          <>
+            <Container>
+              <div className="relative flex items-center justify-between h-16">
+                <div className="flex items-center sm:hidden">
+                  {/* Mobile menu button*/}
+                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md text-gray-900 hover:text-white hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    <Icon
+                      className="block h-6 w-6 text-current"
                       aria-hidden="true"
                     />
-                  ) : (
-                    <MenuIcon
-                      className="block h-6 w-6 text-gray-900"
-                      aria-hidden="true"
-                    />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="hidden sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <Link key={item.name} href={item.href}>
-                        <a
-                          className={clsx([
-                            router.pathname === item.href
-                              ? 'bg-gray-900 text-white'
-                              : 'text-gray-900 hover:bg-gray-900 hover:text-white',
-                            'px-3 py-2 rounded-md text-sm font-medium'
-                          ])}
-                          aria-current={
-                            router.pathname === item.href ? 'page' : undefined
-                          }
-                        >
-                          {item.name}
-                        </a>
-                      </Link>
-                    ))}
+                  </Disclosure.Button>
+                </div>
+                <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                  <div className="hidden sm:block">
+                    <div className="flex space-x-4">
+                      {navigation.map((item) => (
+                        <Link key={item.name} href={item.href}>
+                          <a
+                            className={clsx([
+                              router.pathname === item.href
+                                ? 'bg-gray-900 text-white'
+                                : 'text-gray-900 hover:bg-gray-900 hover:text-white',
+                              'px-3 py-2 rounded-md text-sm font-medium'
+                            ])}
+                            aria-current={
+                              router.pathname === item.href ? 'page' : undefined
+                            }
+                          >
+                            {item.name}
+                          </a>
+                        </Link>
+                      ))}
+                    </div>
                   </div>
                 </div>
+                <div className="w-12 max-w md:max-w-xs lg:max-w-lg md:w-full">
+                  <Autocomplete />
+                </div>
               </div>
-              <div className="w-12 max-w md:max-w-xs lg:max-w-lg md:w-full">
-                <Autocomplete />
-              </div>
-            </div>
-          </Container>
-          <Transition
-            show={open}
-            enter="transition duration-100 ease-out"
-            enterFrom="transform scale-95 opacity-0"
-            enterTo="transform scale-100 opacity-100"
-            leave="transition duration-75 ease-out"
-            leaveFrom="transform scale-100 opacity-100"
-            leaveTo="transform scale-95 opacity-0"
-          >
-            <Disclosure.Panel
-              as="ul"
-              className="sm:hidden z-50 absolute w-full bg-white px-2 pt-2 pb-3 space-y-1 shadow-md"
-              unmount
+            </Container>
+            <Transition
+              show={open}
+              enter="transition duration-100 ease-out"
+              enterFrom="transform scale-95 opacity-0"
+              enterTo="transform scale-100 opacity-100"
+              leave="transition duration-75 ease-out"
+              leaveFrom="transform scale-100 opacity-100"
+              leaveTo="transform scale-95 opacity-0"
             >
-              {navigation.map((item) => (
-                <li key={item.name}>
-                  <Link href={item.href}>
-                    <a
-                      className={clsx([
-                        router.pathname === item.href
-                          ? 'bg-gray-900 text-white'
-                          : 'text-gray-900 hover:bg-gray-900 hover:text-white',
-                        'block px-3 py-2 rounded-md text-base font-medium'
-                      ])}
-                      aria-current={
-                        router.pathname === item.href ? 'page' : undefined
-                      }
-                    >
-                      {item.name}
-                    </a>
-                  </Link>
-                </li>
-              ))}
-            </Disclosure.Panel>
-          </Transition>
-        </>
-      )}
+              <Disclosure.Panel
+                as="ul"
+                className="sm:hidden z-50 absolute w-full bg-white px-2 pt-2 pb-3 space-y-1 shadow-md"
+                unmount
+              >
+                {navigation.map((item) => (
+                  <li key={item.name}>
+                    <Link href={item.href}>
+                      <a
+                        className={clsx([
+                          router.pathname === item.href
+                            ? 'bg-gray-900 text-white'
+                            : 'text-gray-900 hover:bg-gray-900 hover:text-white',
+                          'block px-3 py-2 rounded-md text-base font-medium'
+                        ])}
+                        aria-current={
+                          router.pathname === item.href ? 'page' : undefined
+                        }
+                      >
+                        {item.name}
+                      </a>
+                    </Link>
+                  </li>
+                ))}
+              </Disclosure.Panel>
+            </Transition>
+          </>
+        )
+      }}
     </Disclosure>
   )
 }
