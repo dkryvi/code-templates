83a9a97dd05cc1cde4ed1e3b850987755219c3cf
dkryvi <krivichaninds@gmail.com>
2021-10-06 02:04:18 +0300

    Fix posts sync

--- scripts/sync-posts.ts
+++ scripts/sync-posts.ts
@@ -1,55 +1,62 @@
 import logger from 'loglevel'
 
 import {prisma, getPosts} from '../api'
-import {Post} from '../types'
 
-function getPostData(post: Post, action: 'update' | 'create') {
-  return {
-    author: {
-      [action]: {
-        name: post.author.name,
-        picture: post.author.picture
-      }
-    },
-    content: post.content,
-    coverImage: post.coverImage,
-    date: post.date,
-    excerpt: post.excerpt,
-    slug: post.slug,
-    tags: post.tags,
-    title: post.title,
-    ogImage: {
-      [action]: {
-        url: post.ogImage.url
-      }
-    }
-  }
-}
-
-async function main() {
+async function syncPosts() {
   await prisma.$connect()
 
   const posts = getPosts()
 
-  const promises = posts.map(async function (post) {
-    return await prisma.post.upsert({
-      where: {slug: post.slug},
-      create: getPostData(post, 'create'),
-      update: getPostData(post, 'update')
+  return await Promise.all(
+    posts.map(async function (post) {
+      const dbAuthor = await prisma.author.upsert({
+        where: {name: post.author.name},
+        create: {name: post.author.name, picture: post.author.picture},
+        update: {}
+      })
+      const dbOgImage = await prisma.ogImage.upsert({
+        where: {url: post.ogImage.url},
+        create: {url: post.ogImage.url},
+        update: {}
+      })
+
+      const postData = {
+        author: {
+          connect: {
+            id: dbAuthor.id
+          }
+        },
+        content: post.content,
+        coverImage: post.coverImage,
+        date: post.date,
+        excerpt: post.excerpt,
+        slug: post.slug,
+        tags: post.tags,
+        title: post.title,
+        ogImage: {
+          connect: {
+            id: dbOgImage.id
+          }
+        }
+      }
+
+      return await prisma.post.upsert({
+        where: {slug: post.slug},
+        create: postData,
+        update: postData
+      })
     })
-  })
-
-  const newPosts = await Promise.all(promises)
-
-  logger.info(`🎉 Successfully synced ${newPosts.length} posts`)
+  )
 }
 
-logger.setLevel('info')
-
 try {
-  main()
+  logger.setLevel('info')
+  syncPosts().then((posts) => {
+    logger.info(`🎉 Successfully synced ${posts.length} posts`)
+  })
 } catch (error) {
-  console.log('error')
+  console.log({error})
+  // TODO: use some tool to track errors
   throw error
 } finally {
   prisma.$disconnect()
