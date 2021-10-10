5f6ed4311f9441061dd02ed08dad58cde1f0cb98
dkryvi <krivichaninds@gmail.com>
2021-10-10 16:01:56 +0300

    Update logic to highlight nav item

--- components/nav.tsx
+++ components/nav.tsx
@@ -3,15 +3,23 @@ import {MenuIcon, XIcon} from '@heroicons/react/outline'
 import clsx from 'clsx'
 import Link from 'next/link'
 import {useRouter} from 'next/router'
-import React, {Fragment} from 'react'
+import React from 'react'
 
 import Autocomplete from 'components/autocomplete'
 import Container from 'components/container'
 
 const navigation = [
-  {name: 'Home', href: '/'},
-  {name: 'Collections', href: '/collections'},
-  {name: 'Posts', href: '/posts'}
+  {name: 'Home', href: '/', isActive: (path: string): boolean => path === '/'},
+  {
+    name: 'Collections',
+    href: '/collections',
+    isActive: (path: string): boolean => path.includes('/collections')
+  },
+  {
+    name: 'Posts',
+    href: '/posts',
+    isActive: (path: string): boolean => path.includes('/posts')
+  }
 ]
 
 const Nav: React.FC = () => {
@@ -46,7 +54,7 @@ const Nav: React.FC = () => {
                         <Link key={item.name} href={item.href}>
                           <a
                             className={clsx([
-                              router.pathname === item.href
+                              item.isActive(router.pathname)
                                 ? 'bg-gray-900 text-white'
                                 : 'text-gray-900 hover:bg-gray-600 hover:text-white',
                               'px-3 py-2 rounded-md text-sm font-medium'
