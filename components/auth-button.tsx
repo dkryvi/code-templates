f86afc36d3a7e4a2e790caa1a17f0c083fd2be5d
dkryvi <krivichaninds@gmail.com>
2021-10-15 17:54:04 +0300

    Hide prisma adapter temporary

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -1,4 +1,5 @@
-import {signIn, signOut, useSession} from 'next-auth/client'
+import {useSession} from 'next-auth/client'
+import Link from 'next/link'
 import React from 'react'
 
 import {GithubIcon} from '@icons'
@@ -6,27 +7,23 @@ import {GithubIcon} from '@icons'
 const AuthButton: React.FC = () => {
   const [session] = useSession()
 
-  const handleSignInClick = () => signIn()
-  const handleSignOutClick = () => signOut()
-
   return session ? (
     <>
       <p>Signed in as {session?.user?.email}</p>
-      <button className="text-gray-900" onClick={handleSignInClick}>
-        Sign Out
-      </button>
+      <Link href="/api/auth/signout">
+        <a className="text-gray-900">Sign Out</a>
+      </Link>
     </>
   ) : (
-    <button
-      className="group flex align-center py-2 px-6 prose bg-gray-900 hover:bg-white hover:text-gray-900 border border-gray-900 text-white font-bold duration-200 transition-colors"
-      onClick={handleSignOutClick}
-    >
-      <GithubIcon
-        className="mr-4 text-white group-hover:text-gray-900"
-        fill="currentColor"
-      />
-      Sign in with GitHub
-    </button>
+    <Link href="/api/auth/signin">
+      <a className="group flex align-center py-2 px-6 prose bg-gray-900 hover:bg-white hover:text-gray-900 border border-gray-900 text-white font-bold duration-200 transition-colors">
+        <GithubIcon
+          className="mr-4 text-white group-hover:text-gray-900"
+          fill="currentColor"
+        />
+        Sign in with GitHub
+      </a>
+    </Link>
   )
 }
 
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,16 +1,25 @@
+import {NextApiHandler} from 'next'
 import NextAuth from 'next-auth'
-import Adapters from 'next-auth/adapters'
+// import Adapters from 'next-auth/adapters'
 import Providers from 'next-auth/providers'
 
-import prisma from '../../../lib/prisma'
+// import prisma from '../../../lib/prisma'
 
-export default NextAuth({
+const options = {
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
     })
   ],
-  adapter: Adapters.Prisma.Adapter({prisma}),
-  secret: process.env.SECRET
-})
+  database: process.env.DATABASE_URL,
+  // adapter: Adapters.Prisma.Adapter({prisma}),
+  secret: process.env.SECRET,
+  session: {
+    jwt: true
+  }
+}
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options)
+
+export default authHandler
