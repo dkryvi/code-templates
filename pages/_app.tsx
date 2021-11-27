d6581eb0a83b634b232deaf1c77862466e83df20
dkryvi <krivichaninds@gmail.com>
2021-11-27 15:38:45 +0300

    Add toastify snackbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import {DefaultSeo} from 'next-seo'
 import {ThemeProvider} from 'next-themes'
 import {AppProps} from 'next/app'
+import {ToastContainer, Slide} from 'react-toastify'
 
 import 'styles/index.css'
 
@@ -12,6 +13,18 @@ export default function MyApp({
 }: AppProps): React.ReactElement {
   return (
     <ThemeProvider>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        transition={Slide}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <DefaultSeo {...DEFAULT_SEO} />
       <Component {...pageProps} />
     </ThemeProvider>
