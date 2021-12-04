6e679236391e35525ea5b8dd2815c4c0ce62bd7f
dkryvi <krivichaninds@gmail.com>
2021-12-04 09:28:46 +0300

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import {DefaultSeo} from 'next-seo'
 import {ThemeProvider} from 'next-themes'
 import {AppProps} from 'next/app'
+import Script from 'next/script'
 import {ToastContainer, Slide} from 'react-toastify'
 
 import 'styles/index.css'
@@ -12,21 +13,39 @@ export default function MyApp({
   pageProps
 }: AppProps): React.ReactElement {
   return (
-    <ThemeProvider>
-      <ToastContainer
-        position="bottom-center"
-        autoClose={5000}
-        hideProgressBar
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        transition={Slide}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
-      <DefaultSeo {...DEFAULT_SEO} />
-      <Component {...pageProps} />
-    </ThemeProvider>
+
+      <Script strategy="lazyOnload" id="data-layer">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
+      <ThemeProvider>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          transition={Slide}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <DefaultSeo {...DEFAULT_SEO} />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   )
 }
