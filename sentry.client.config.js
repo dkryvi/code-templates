4f3d2956d7e02e479dd4ed0561e6d69a028b29c2
dkryvi <krivichaninds@gmail.com>
2022-06-04 10:50:04 +0300

    fix: update sentry env

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,11 +1,7 @@
 import * as Sentry from '@sentry/nextjs'
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
-
 Sentry.init({
-  dsn:
-    SENTRY_DSN ||
-    'https://b2f10b0f552a4421be4365a429cf9d32@o1068770.ingest.sentry.io/6063022',
+  dsn: process.env.SENTRY_DSN,
   tracesSampleRate: 1.0,
   beforeSend:
     process.env.NODE_ENV !== 'production'
